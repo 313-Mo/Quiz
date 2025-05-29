1ad87c64c28d252ada0b28fe962fe006833d910a
Oskar Schießl <oskar.schiessl@studium.th-luebeck.de>
2025-05-29 10:12:34 +0200

    test: ActiveProfiles("test") bei Integration Test

--- server/QuizMaster/src/test/java/Quiz/Master/Group/QuizMaster/QuizIntegrationTest.java
+++ server/QuizMaster/src/test/java/Quiz/Master/Group/QuizMaster/QuizIntegrationTest.java
@@ -6,6 +6,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import Quiz.Master.Group.QuizMaster.Entities.Category;
 import Quiz.Master.Group.QuizMaster.Entities.MultipleChoiceQuiz;
@@ -13,6 +14,7 @@ import Quiz.Master.Group.QuizMaster.Entities.Question;
 import Quiz.Master.Group.QuizMaster.Repositories.QuestionRepository;
 import Quiz.Master.Group.QuizMaster.Repositories.QuizRepository;
 
+@ActiveProfiles("test")
 @SpringBootTest
 public class QuizIntegrationTest {
 
