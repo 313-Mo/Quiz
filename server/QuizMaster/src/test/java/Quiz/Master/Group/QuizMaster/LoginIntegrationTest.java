6bfbbac57a784de35b3da35fb51421c839eeb274
Oskar Schießl <oskar.schiessl@studium.th-luebeck.de>
2025-06-01 17:29:17 +0200

    fix: tests bearbeitet und fehlerhaften Test gelöscht

--- server/QuizMaster/src/test/java/Quiz/Master/Group/QuizMaster/LoginIntegrationTest.java
+++ server/QuizMaster/src/test/java/Quiz/Master/Group/QuizMaster/LoginIntegrationTest.java
@@ -8,11 +8,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+@ActiveProfiles("test")
 @WebMvcTest(controllers = QuizUserController.class)
 public class LoginIntegrationTest {
 
--- server/QuizMaster/src/test/java/Quiz/Master/Group/QuizMaster/QuizIntegrationTest.java
+++ server/QuizMaster/src/test/java/Quiz/Master/Group/QuizMaster/QuizIntegrationTest.java
@@ -6,7 +6,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 import Quiz.Master.Group.QuizMaster.Entities.Category;
 import Quiz.Master.Group.QuizMaster.Entities.MultipleChoiceQuiz;
@@ -14,7 +13,7 @@ import Quiz.Master.Group.QuizMaster.Entities.Question;
 import Quiz.Master.Group.QuizMaster.Repositories.QuestionRepository;
 import Quiz.Master.Group.QuizMaster.Repositories.QuizRepository;
 
-@ActiveProfiles("test")
+
 @SpringBootTest
 public class QuizIntegrationTest {
 
